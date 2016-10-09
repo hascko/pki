937640365ddf2c681d32921aea5ca3240e57a37f
hascko <sangare@intechinfo.fr>
2016-10-09 14:30:59 +0200

    Ajout de deux nouveaux scripts revocation serveur et client

---
+++ revocation_client.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+file1=$1
+nom_certif=$2
+nb_files=$(find ./$nom_certif/newcerts/ -type f | wc -l)
+
+i=0
+
+while [ "$i" -lt "$nb_files" ]
+do
+let i++
+fichier=0$i.pem
+cmp -s ./$nom_certif/$file1.pem ./$nom_certif/newcerts/$fichier
+
+if [ $? -eq 0 ];then
+	openssl ca -config ./openssl.cnf -name CA_$nom_certif -revoke ./$nom_certif/newcerts/$fichier
+	openssl ca -gencrl -config ./openssl.cnf -extensions CLIENT_RSA_SSL -name CA_$nom_certif -crldays 1 -out ./$nom_certif/crl.pem
+	openssl crl -in ./$nom_certif/crl.pem -text -noout
+fi
+let i--
+
+let i++
+done
