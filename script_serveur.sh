659f198f24649f26994b31a7eca836e1bda036f0
hascko <sangare@intechinfo.fr>
2016-10-11 01:32:09 +0200

    Ajout dans le script pour récupérer les information supplémentaires

--- script_serveur.sh
+++ script_serveur.sh
@@ -15,6 +15,12 @@ email=$6
 
 repertoire=/opt/rootpki
 
+
+pays=$(grep "countryName_default" $repertoire/$nom_certif/openssl.cnf | cut -d" " -f2)
+departement=$(grep "stateOrProvinceName_default" $repertoire/$nom_certif/openssl.cnf | cut -d" " -f2)
+ville=$(grep "localityName_default" $repertoire/$nom_certif/openssl.cnf | cut -d" " -f2)
+organisation=$(grep "organizationName_default" $repertoire/$nom_certif/openssl.cnf | cut -d" " -f2)
+
 #On créé le répertoire du serveur
 mkdir $repertoire/$nom_certif/$nom_serveur
 echo "$password_certif_fille" > $repertoire/$nom_certif/ca.pass
@@ -25,7 +31,7 @@ $password_serveur
 EOF
 
 # On cree un certificat non signe
-openssl req -new -nodes -batch -newkey rsa:1024 -keyout $repertoire/$nom_certif/$nom_serveur/$nom_serveur.key -out $repertoire/$nom_certif/$nom_serveur/$nom_serveur.crs -config $repertoire/$nom_certif/openssl.cnf -subj "/CN=$cname/emailAddress=$email"
+openssl req -new -nodes -batch -newkey rsa:1024 -keyout $repertoire/$nom_certif/$nom_serveur/$nom_serveur.key -out $repertoire/$nom_certif/$nom_serveur/$nom_serveur.crs -config $repertoire/$nom_certif/openssl.cnf -subj "/C=$pays/ST=$departement/L=$ville/O=$organisation/CN=$cname/emailAddress=$email"
 
 # On signe avec la cle privee CA
 openssl ca -passin file:$repertoire/$nom_certif/ca.pass -batch -name $nom_certif -config $repertoire/$nom_certif/openssl.cnf -extensions SERVER_RSA_SSL -out $repertoire/$nom_certif/$nom_serveur/$nom_serveur.pem -infiles $repertoire/$nom_certif/$nom_serveur/$nom_serveur.crs
